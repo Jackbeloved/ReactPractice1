34f6804c5c1b8f91cffdfa9d7747e4abdb239116
jackzhou <jzhou@huonaqua.com.au>
2021-10-26 17:01:10 +1100

    Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Tweet from "./Tweet"
+// import "./App.css"
 
-function App() {
+function App(){
+
+const [isRed,setRed] = useState(false);
+const [count,setCount] = useState(0);
+
+
+const increment =() => {
+  setCount(count + 1);
+  setRed(!isRed);
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="app">
+      <h1 className={isRed ? 'red':'blue'}>Change color</h1>
+      <button onClick={increment} className="button">Increment</button>
+      <h1>{count}</h1>
 
+    </div>
+  )
+}
 export default App;
